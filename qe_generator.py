1e2ac834e0d0279aa7e934f44cb82fbdddd233c5
HABIB UR REHMAAN <h.rehmaan96@gmail.com>
2026-05-02 17:24:32 +0200

    Refactor generator to use GUI-provided values

--- qe_generator.py
+++ qe_generator.py
@@ -1,2 +1,88 @@
 def generate_qe_input():
     return "Quantum ESPRESSO input file will be generated here."
+"""
+Quantum ESPRESSO input generator.
+
+This module contains helper functions for generating pw.x input files.
+The values are expected to come from the GUI in app.py.
+"""
+
+
+def bool_to_qe(value):
+    """
+    Convert Python True/False values to Quantum ESPRESSO .true. / .false.
+    """
+    return ".true." if value else ".false."
+
+
+def generate_qe_input(
+    calculation,
+    verbosity,
+    restart_mode,
+    pseudo_dir,
+    tstress,
+    tprnfor,
+    ecutwfc,
+    ecutrho,
+    occupations,
+    degauss,
+    smearing,
+    nspin,
+    ntyp,
+    nat,
+    ibrav,
+    mixing_mode,
+    mixing_beta,
+    diagonalization,
+    atomic_species,
+    cell_parameters,
+    atomic_positions,
+    k_points,
+):
+    """
+    Generate a Quantum ESPRESSO pw.x input file as text.
+
+    All values should be provided by the GUI.
+    """
+
+    tstress_value = bool_to_qe(tstress)
+    tprnfor_value = bool_to_qe(tprnfor)
+
+    qe_input = f"""&CONTROL
+    calculation = '{calculation}'
+    verbosity = '{verbosity}'
+    restart_mode = '{restart_mode}'
+    tstress = {tstress_value}
+    tprnfor = {tprnfor_value}
+    pseudo_dir = '{pseudo_dir}'
+/
+&SYSTEM
+    ecutwfc = {ecutwfc}
+    ecutrho = {ecutrho}
+    occupations = '{occupations}'
+    degauss = {degauss}
+    smearing = '{smearing}'
+    nspin = {nspin}
+    ntyp = {ntyp}
+    nat = {nat}
+    ibrav = {ibrav}
+/
+&ELECTRONS
+    mixing_mode = '{mixing_mode}'
+    mixing_beta = {mixing_beta}
+    diagonalization = '{diagonalization}'
+/
+ATOMIC_SPECIES
+{atomic_species}
+
+CELL_PARAMETERS angstrom
+{cell_parameters}
+
+ATOMIC_POSITIONS angstrom
+{atomic_positions}
+
+K_POINTS automatic
+{k_points}
+"""
+
+    return qe_input
